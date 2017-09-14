cfa9199aed6ee277325ae2bba0cf7f444d3e2403
icodecloud <icodecloud@163.com>
2017-09-14 10:35:43 +0800

    login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -4,7 +4,7 @@ import {Form, Icon, Input, Button, Checkbox} from 'antd';
 const FormItem = Form.Item;
 const {create} = Form;
 
-import './Login.less';
+import styles from './Login.less';
 
 @connect((state) => {
   return {...state.app.login};
@@ -32,22 +32,21 @@ export default class Login extends React.Component {
     });
   }
   render = () => {
-    console.log('this.props===', this.props);
-    const {getFieldDecorator} = this.props.form;
+    const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
+      <Form onSubmit={this.handleSubmit} className={styles['login-form']}>
         <FormItem>
           {getFieldDecorator('userName', {
-            rules: [{required: true, message: 'Please input your username!'}],
+            rules: [{ required: true, message: 'Please input your username!' }],
           })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username"/>
+            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
           )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('password', {
-            rules: [{required: true, message: 'Please input your Password!'}],
+            rules: [{ required: true, message: 'Please input your Password!' }],
           })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password"/>
+            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
           )}
         </FormItem>
         <FormItem>
@@ -57,8 +56,8 @@ export default class Login extends React.Component {
           })(
             <Checkbox>Remember me</Checkbox>
           )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
+          <a className={styles['login-form-forgot']} href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" className={styles['login-form-button']}>
             Log in
           </Button>
           Or <a href="">register now!</a>
